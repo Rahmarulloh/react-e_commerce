80c13c307578664417bda289d48feccd2c00cf36
komolxoja <komolxoja2007@gmail.com>
2023-11-08 23:06:52 +0500

    filter qilindi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import ProductList from "components/product/productList";
 import "assets/styles/main.scss";
+import Sidebar from "components/common/sidebar";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <ProductList />
+      {/* <ProductList /> */}
+      <Sidebar />
     </div>
   );
 }
