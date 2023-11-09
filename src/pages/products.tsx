3eaeeabb3b8fa6c770c2e3059f5ae26c64f4d9ec
Uzgur <uzbmv2@gmail.com>
2023-11-09 17:43:40 +0500

    shaxboz jamoaga qo'shildi

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,20 @@
 import Sidebar from "components/common/sidebar";
 import ProductList from "components/product/productList";
+import products from "utils/api";
+import { IProduct } from "utils/types";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+  const [productsArr, setProductsArr] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const data = await products();
+      setProductsArr(data.sort((a, b) => a.price - b.price));
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <div className="products">
       <Sidebar />
